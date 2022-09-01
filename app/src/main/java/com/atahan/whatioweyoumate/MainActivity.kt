09b0225dcea8660223b2f46dbadf79b090792b8a
cenk atahan özbek <atahanozzbek@gmail.com>
2022-09-01 17:46:43 +0300

    add dialog to add a person

--- app/src/main/java/com/atahan/whatioweyoumate/MainActivity.kt
+++ app/src/main/java/com/atahan/whatioweyoumate/MainActivity.kt
@@ -1,17 +1,75 @@
 package com.atahan.whatioweyoumate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.atahan.whatioweyoumate.databinding.ActivityMainBinding
+import com.atahan.whatioweyoumate.databinding.LayoutDialogCreateGroupBinding
+import com.atahan.whatioweyoumate.model.Person
+
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+//    private var groupSize = 0
+    private lateinit var friendGroup: ArrayList<Person>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        friendGroup = ArrayList()
+        binding.btnCreate.setOnClickListener {
+            openDialog()
+        }
     }
-    
+
+    private fun openDialog() {
+        val dialog = Dialog(this)
+        val bindingDialog = LayoutDialogCreateGroupBinding.inflate(layoutInflater)
+
+        var name = ""
+        var payment = 0
+
+        bindingDialog.btnConfirm.setOnClickListener {
+//            if (bindingDialog.etName.text.toString() == "") {
+//                return@setOnClickListener
+//            }
+//
+//            if (bindingDialog.etPayment.text.toString() == "") {
+//                payment = 0
+//                friendGroup.add(
+//                    Person(
+//                        bindingDialog.etName.text.toString(),
+//                        payment
+//                    )
+//                )
+//                dialog.dismiss()
+//            }
+
+                bindingDialog.etName.text?.let {
+                name = it.toString()
+            }
+
+            bindingDialog.etPayment.text?.let {
+                payment = it.toString().toInt()
+            }
+
+            friendGroup.add(Person(name, payment))
+            dialog.dismiss()
+        }
+
+        bindingDialog.btnDismiss.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.apply {
+            setContentView(bindingDialog.root)
+            show()
+        }
+    }
 }
---
+++ app/src/main/java/com/atahan/whatioweyoumate/model/Person.kt
@@ -0,0 +1,6 @@
+package com.atahan.whatioweyoumate.model
+
+data class Person(
+    val name: String,
+    val payment: Int
+)
