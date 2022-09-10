03f92381f7b5f1abc9ad656b5ba376e7e7a153b1
cenk atahan özbek <atahanozzbek@gmail.com>
2022-09-10 16:58:44 +0300

    [Description]: remove unused imports

--- app/src/main/java/com/atahan/whatioweyoumate/MainActivity.kt
+++ app/src/main/java/com/atahan/whatioweyoumate/MainActivity.kt
@@ -2,9 +2,7 @@ package com.atahan.whatioweyoumate
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.atahan.whatioweyoumate.databinding.ActivityMainBinding
