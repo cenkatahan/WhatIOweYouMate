40495ace5be69adce36e1e197586c2748278854b
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-30 01:00:01 +0300

    [Description]: add repository.

---
+++ app/src/main/java/com/atahan/whatioweyoumate/repository/FriendRepository.kt
@@ -0,0 +1,18 @@
+package com.atahan.whatioweyoumate.repository
+
+import com.atahan.whatioweyoumate.db.FriendDao
+import com.atahan.whatioweyoumate.model.Friend
+import javax.inject.Inject
+
+class FriendRepository
+@Inject constructor(
+    private val friendDao: FriendDao
+) {
+
+    fun add(friend: Friend) = friendDao.save(friend)
+    fun remove(friend: Friend) = friendDao.delete(friend)
+    fun update(friend: Friend) = friendDao.update(friend)
+    fun getFriends() = friendDao.getFriends()
+    fun removeFriends() = friendDao.deleteAll()
+
+}
