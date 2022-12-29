2ad7fb978af37d6df63017fea63827d18d1e2200
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-30 00:58:43 +0300

    [Description]: fix friend data class.

--- app/src/main/java/com/atahan/whatioweyoumate/model/Friend.kt
+++ app/src/main/java/com/atahan/whatioweyoumate/model/Friend.kt
@@ -3,10 +3,13 @@ package com.atahan.whatioweyoumate.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.atahan.whatioweyoumate.common.Constants.COL_NAME
+import com.atahan.whatioweyoumate.common.Constants.COL_PAYMENT
+import com.atahan.whatioweyoumate.common.Constants.TABLE_FRIEND
 
-@Entity(tableName = "")
+@Entity(tableName = TABLE_FRIEND)
 data class Friend(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "payment") val payment: Int
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = COL_NAME) val name: String = "",
+    @ColumnInfo(name = COL_PAYMENT) val payment: Int = 0
 )
