2b17fb07958d80e85ca79aae58aec719f5c2e91d
cenk atahan özbek <atahanozzbek@gmail.com>
2022-12-30 00:59:07 +0300

    [Description]: add constants for roomDb.

---
+++ app/src/main/java/com/atahan/whatioweyoumate/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.atahan.whatioweyoumate.common
+
+object Constants {
+    const val TABLE_FRIEND = "friend_table"
+    const val COL_ID = "id"
+    const val COL_NAME = "name"
+    const val COL_PAYMENT = "payment"
+}
