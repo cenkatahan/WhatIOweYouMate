899ac9255be5c24ad71adeb3b7136f367ccad685
cenk atahan özbek <atahanozzbek@gmail.com>
2023-01-12 11:31:21 +0300

    [Description]: implement parcelable and fix graph.

--- app/src/main/java/com/atahan/whatioweyoumate/model/Friend.kt
+++ app/src/main/java/com/atahan/whatioweyoumate/model/Friend.kt
@@ -1,15 +1,18 @@
 package com.atahan.whatioweyoumate.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.atahan.whatioweyoumate.common.Constants.COL_NAME
 import com.atahan.whatioweyoumate.common.Constants.COL_PAYMENT
 import com.atahan.whatioweyoumate.common.Constants.TABLE_FRIEND
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = TABLE_FRIEND)
 data class Friend(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = COL_NAME) val name: String = "",
     @ColumnInfo(name = COL_PAYMENT) val payment: Int = 0
-)
+): Parcelable
